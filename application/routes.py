8de0a59c66cf340a687d609872287e6b59103b44
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-07-21 16:18:39 +0530

    Routes and additional pages

--- application/routes.py
+++ application/routes.py
@@ -6,5 +6,18 @@ from application import app
 def redir():
     return redirect('/index')
 @app.route("/index")
+@app.route("/home")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',login=False)
+
+@app.route("/login")
+def login():
+    return render_template('login.html',login=False)
+
+@app.route("/events")
+def events():
+    return render_template('events.html',login=False)
+
+@app.route("/register")
+def register():
+    return render_template('register.html',login=False)
